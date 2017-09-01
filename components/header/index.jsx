89907fe018d00380b59fd9eef6108142e0ead92a
zappjs-test <31462589+zappjs-test@users.noreply.github.com>
2017-09-01 02:32:07 -0500

    generates files

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -4,4 +4,12 @@ import { connect } from 'react-redux';
 class Header extends Component {
 }
 
+Header.defaultProps({
+    username: ''
+});
+
+Header.propTypes = {
+    username: PropTypes.string
+};
+
 export default connect()(Home);
